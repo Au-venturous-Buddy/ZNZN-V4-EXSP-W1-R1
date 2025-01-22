aeb42f2ed9ec7722a1f9d4d37b0f96f12b3cf719
Au-venturous-Buddy <130553802+Au-venturous-Buddy@users.noreply.github.com>
2025-01-22 16:52:00 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/Au-Some Blogs.png`, // This path is relative to the root of the site.
+        icon: `src/images/ZNZNV4.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
